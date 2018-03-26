bbc1ad41851629c6327bcedca83f12fbe4e13b58
BoyLondon <fucshu@163.com>
2018-03-27 00:09:46 +0800

    add create user route

--- AutoSummarization/routes/user.py
+++ AutoSummarization/routes/user.py
@@ -5,11 +5,22 @@ from flask import request, Blueprint, jsonify
 
 user_bp = Blueprint("user", __name__)
 
-@user_bp.route("/login",methods=["POST"])
+
+@user_bp.route("/login", methods=["POST"])
 def user_login():
-    username = request.json.get("username",None)
-    password = request.json.get("password",None)
-    res = auth.auth_password(username,password)
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    res = auth.auth_password(username, password)
+    return jsonify({
+        "code": "1",
+        "data": res
+    })
+
+
+@user_bp.route("/create", methods=["POST"])
+def create_user():
+    data = {key: value for key, value in request.json.iteritems()}
+    res = auth.create_user(data)
     return jsonify({
         "code": "1",
         "data": res
