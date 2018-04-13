02d8032759047753613c3e3e755bedd19ff74058
BoyLondon <fucshu@163.com>
2018-04-13 13:03:47 +0800

    update textrank

--- AutoSummarization/controllers/textrank.py
+++ AutoSummarization/controllers/textrank.py
@@ -2,7 +2,12 @@
 # __author__=''
 from AutoSummarization.models.entities import Textrank
 from AutoSummarization.controllers import session_scope
+from zhon.hanzi import punctuation
+import re
+import jieba.posseg as pseg
 
+
+# TODO punctuation add
 
 def textrank_history():
     with session_scope() as db_session:
@@ -20,3 +25,31 @@ def textrank_history():
 
 def get_summary(data):
     return None
+
+
+def textrank_stemming(doc):
+    sentences = re.sub(ur"[%s]+" % punctuation, "\n", doc)
+    sentences_list = []
+    append_sen = ""
+    for sen in sentences:
+        if sen == '\n':
+            sentences_list.append(append_sen)
+            append_sen = ""
+            continue
+        append_sen += sen
+    return sentences_list
+
+
+def textrank_participle(sentences_list):
+    res = []
+    for sentence in sentences_list:
+        part = pseg.cut(sentence)
+        part_res = []
+        for word, property in part:
+            if property == "a" or property == "an" or property == "n" or property == "v":
+                part_res.append(word)
+
+        res.append(part_res)
+    return res
+
+
